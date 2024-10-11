3a73dcbaa64d1555b947bce65025cbed59045423
Stefano Trejo <stides11@gmail.com>
2024-10-11 13:03:41 -0700

    added chat screen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import React, { useState } from 'react'
 import Startscreen from './components/Startscreen'
+import Chatscreen from './components/Chatscreen'
+
 
 function App() {
+  const [isChatting, setIsChatting] = useState(false)
+
+  const handleStartChat = () => {
+    setIsChatting(true)
+  }
 
   return (
     <div className="container">
-    <Startscreen />
+    {isChatting ? (
+      <Chatscreen />
+    ) : ( 
+      <Startscreen onStartChat={handleStartChat} />
+    )}
     </div>
   )
 }
 
+   
 export default App
---
+++ src/components/Chatscreen.jsx
@@ -0,0 +1,14 @@
+import './Chatscreen.css'
+import { useState } from 'react'
+
+import React from 'react'
+
+const Chatscreen = () => {
+  return (
+    <div>
+        <h1>Chatscreen</h1>
+    </div>
+  )
+}
+
+export default Chatscreen
